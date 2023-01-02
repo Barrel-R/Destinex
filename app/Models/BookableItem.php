b84ea8f32bd6209385790b1a0a1aa72ff0522fd9
Henrique Barrel <henriquebarrel@gmail.com>
2023-01-02 20:29:59 -0300

    inital commit

---
+++ app/Models/BookableItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BookableItem extends Model
+{
+    use HasFactory;
+}
